7e20fc9078031180045e5fa9fb933569b2f01d18
Sarah Sotomayor <sarah@i0119Mac.local>
2023-04-19 10:32:22 -0600

    todo list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
+    const [newTodo, setNewTodo] = useState("");
+    const [todos, setTodos] = useState([]);
+
+    const handleNewTodoSubmit = (e) => {
+    e.preventDefault();
+
+    if (newTodo.length == 0) {
+        return;
+    }
+
+    const todoItem = {
+        text: newTodo,
+        complete: false
+    }
+
+    setTodos([...todos, todoItem]);
+    setNewTodo("");
+    };
+
+    const handleTodoDelete = (delIndex) => {
+    const filteredTodos = todos.filter((todos, index) => {
+        return index !== delIndex; 
+    });
+    setTodos(filteredTodos);
+    }
+
+    const handleTodoCheckbox = (i) => {
+    const updatedTodos = todos.map((todos, index) => {
+        if (i == index) {
+        const updatedTodos = {...todos, complete: !todos.complete};
+        return updatedTodos;
+    }
+        return todos;
+    });
+
+    setTodos(updatedTodos);
+    }
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <header>TODO LIST</header>
+
+        <form onSubmit={(e) => {
+        handleNewTodoSubmit(e);}}>
+            <input onChange={(e) => {
+            setNewTodo(e.target.value);
+        }} type="text" />
+            <button>Add</button>
+        </form>
+
+        {todos.map((todo, index) => {
+
+        const todoClasses = ["bold"];
+        
+        if (todo.complete) {
+            todoClasses.push("line-through")
+        }      
+
+        return (
+            <div key={index}>
+            <input type="checkbox" onChange={(e) => {
+                handleTodoCheckbox(index);
+            }}
+                checked={todo.complete}/>
+            <span className={todoClasses.join(" ")}>{todo.text}</span>
+            <button onClick={(e) => {
+                handleTodoDelete(index);
+            }}>Delete</button>
+            </div>  
+        );
+        })}
+    </div> 
+    );
 }
 
 export default App;
